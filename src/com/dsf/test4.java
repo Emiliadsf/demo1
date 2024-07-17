6a355f7c3b6ef836cfb5697eeff1c62e8a9add9f
Emilia <3069464279@qq.com>
2024-07-17 18:12:02 +0800

    hot commit 2

--- src/com/dsf/test4.java
+++ src/com/dsf/test4.java
@@ -4,6 +4,8 @@ public class test4 {
     public static void main(String[] args) {
         System.out.println("hello git33333");
         System.out.println("hello git33333 hot-fix");
-        System.out.println("hello git33333 master1");
+        System.out.println("hello git33333 hot-fix2");
+
+
     }
 }
